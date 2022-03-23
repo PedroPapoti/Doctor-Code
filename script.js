a9011011aaba53a017b8b86e04621b74f9f67d16
PedroPapoti <pedropapoti@gmail.com>
2022-03-23 16:02:27 -0300

    Video
    A video of the code doctor was built in webm format.

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-
 let doctor = [];
 let size = 20;
 let counter = 60;
 let number;
 let second;
+let p5Canvas
+
+let capturer = new CCapture({
+    format: "webm",
+    framerate: 5,
+    verbose: true
+  });
 
 function preload() {
     for(let i=0; i <13; i++){
-        doctor[i] = loadImage('imagens/doctor'+i+'.png');
+        doctor[i] = loadImage('img/doctor'+i+'.png');
     }
 }
 
 function setup() {
-    createCanvas(displayWidth,displayHeight, WEBGL);
+    p5Canvas = createCanvas(displayWidth,displayHeight, WEBGL);
     frameRate(10);
     counter += frameCount;
     number = 0;
@@ -20,6 +26,9 @@ function setup() {
 }
 
 function draw() {
+    if (frameCount === 1){
+        capturer.start();
+      } 
     background(random(40,150));
     rotateZ(frameCount * 0.012);
     rotateX(frameCount * 0.01);
@@ -42,6 +51,14 @@ function draw() {
         if(number === 12){
             number = 0;
         }
+    }
+
+    if(frameCount < 60 * 20) { 
+        capturer.capture(p5Canvas.canvas);
+    }else if(frameCount === 60 * 20) { 
+        noLoop();
+        capturer.stop();
+        capturer.save();
     }
 }
 
